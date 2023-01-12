603d849c8c7c4cb0d6d4ca9c1081105a5224dc2e
nullqwertyuiop <null@member.fsf.org>
2023-01-13 02:53:09 +0800

    :construction: Add Unit Test
    Signed-off-by: nullqwertyuiop <null@member.fsf.org>

---
+++ tests/unit_test.py
@@ -0,0 +1,57 @@
+import os
+import sys
+import unittest
+
+from gameyamlspiderandgenerator.util.spider import get_status, get_text
+
+
+def update_config():
+    from gameyamlspiderandgenerator.util.setting import config, set_config
+
+    config(
+        {
+            "proxy": {
+                "http": "http://127.0.0.1:10809",
+                "https": "http://127.0.0.1:10809",
+            },
+            "api": {
+                "google-play": "a714b00383f0662a61b2e382d55c685f17015617aa7048972da58a756fb75e90",
+                "apple": "a714b00383f0662a61b2e382d55c685f17015617aa7048972da58a756fb75e90",
+            },
+        }
+    )
+    set_config("hook", ["search"])
+    set_config("plugin", ["steam", "itchio"])
+
+
+class CliUnitTest(unittest.TestCase):
+    def test_cli(self):
+        status_code = os.system(
+            f"{sys.executable} -m gameyamlspiderandgenerator.cli url"
+        )
+        status_code = os.waitstatus_to_exitcode(status_code)
+        self.assertEqual(status_code, 0)
+
+
+class InitUnitTest(unittest.TestCase):
+    def test_init(self):
+        from gameyamlspiderandgenerator.plugin.steam import Search as Steam
+
+        update_config()
+        self.assertIsInstance(
+            Steam(
+                "https://store.steampowered.com/app/381210/Dead_by_Daylight/"
+            ).make_yaml(),
+            str,
+        )
+
+
+class SpiderUnitTest(unittest.TestCase):
+    def test_spider(self):
+        update_config()
+        self.assertGreaterEqual(get_status("https://store.steampowered.com/"), 0)
+        self.assertIsInstance(get_text("https://www.so.com/"), str)
+
+
+if __name__ == "__main__":
+    unittest.main()
