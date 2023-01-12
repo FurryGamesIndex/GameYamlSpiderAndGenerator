f8461feca7c44d8ae8aea9a06274bea870619c76
nullqwertyuiop <null@member.fsf.org>
2023-01-13 02:45:34 +0800

    :construction: Use class instead of dict
    Signed-off-by: nullqwertyuiop <null@member.fsf.org>

--- gameyamlspiderandgenerator/util/setting.py
+++ gameyamlspiderandgenerator/util/setting.py
@@ -1,14 +1,21 @@
-setting = {}
+from typing import Dict
+
+
+class Setting:
+    proxy: Dict[str, str] = {}
+    api: Dict[str, str] = {}
+
+
+setting = Setting()
 
 
 def config(data: dict):
-    global setting
-    setting = data
+    setting.__dict__.update(data)
 
 
 def get_config():
-    return setting
+    return setting.__dict__
 
 
 def set_config(name: str, data: dict | list[str]):
-    setting[name] = data
+    setting.__dict__[name] = data
