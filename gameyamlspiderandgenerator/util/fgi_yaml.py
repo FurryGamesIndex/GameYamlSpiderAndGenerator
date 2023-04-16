9560472f9f1bcb745a5df630e6b166a7d9eb8d06
kaixinol <keiplyer@gmail.com>
2023-04-16 10:46:20 +0800

    fix test

--- gameyamlspiderandgenerator/util/fgi_yaml.py
+++ gameyamlspiderandgenerator/util/fgi_yaml.py
@@ -44,7 +44,7 @@ class YamlData:
         self.raw_dict = raw_data
 
     def __str__(self):
-        return dump_to_yaml({**self.raw_dict["thumbnail"], "thumbnail": 'thumbnail.png'})
+        return dump_to_yaml({**self.raw_dict, "thumbnail": 'thumbnail.png'})
 
     def __bytes__(self):
         from ..util.spider import get_bytes
@@ -54,6 +54,6 @@ class YamlData:
         zip_data.writestr('thumbnail.png', process_thumbnail(img))
 
         zip_data.writestr(self.raw_dict['name'] + ".yaml",
-                          dump_to_yaml({**self.raw_dict["thumbnail"], "thumbnail": 'thumbnail.png'}))
+                          dump_to_yaml({**self.raw_dict, "thumbnail": 'thumbnail.png'}))
         zip_data.close()
         return _io.getvalue()
