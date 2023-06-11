4bdd9382f74cdad0dac24a23925ae2ea49c48e95
kaixinol <keiplyer@gmail.com>
2023-06-11 18:36:42 +0800

    rewrite unit tests

--- tests/unit_test.py
+++ tests/unit_test.py
@@ -19,7 +19,7 @@ class CliUnitTest(unittest.TestCase):
         result = subprocess.run([sys.executable, "-m", "gameyamlspiderandgenerator"])
         self.assertGreaterEqual(result.returncode, 1)
 
-    def test_cli(self):
+    def test_steam(self):
         result = subprocess.run(
             [
                 sys.executable,
@@ -31,36 +31,26 @@ class CliUnitTest(unittest.TestCase):
         )
         self.assertEqual(result.returncode, 0)
 
+    def test_itchio(self):
+        result = subprocess.run(
+            [
+                sys.executable,
+                "-m",
+                "gameyamlspiderandgenerator",
+                "https://fymm-game.itch.io/ddp",
+                "--fast"
+            ]
+        )
+        self.assertEqual(result.returncode, 0)
+
 
 class SpiderUnitTest(unittest.TestCase):
     """Rewritten from test_spider.py"""
 
     def test_spider(self):
         from gameyamlspiderandgenerator.util.spider import get_status, get_text
-        self.assertGreaterEqual(get_status("https://store.steampowered.com/"), 200)
-        self.assertIsInstance(get_text("https://www.so.com/"), str)
-
-
-class SteamUnitTest(unittest.TestCase):
-    """Rewritten from plugin/steam.py"""
-
-    def test_steam(self):
-        from gameyamlspiderandgenerator.plugin.steam import Steam
-        steam = Steam("https://store.steampowered.com/app/381210/Dead_by_Daylight/")
-        self.assertIsInstance(
-            str(steam.to_yaml()),
-            str,
-        )
-
-
-class ItchIOUnitTest(unittest.TestCase):
-    def test_itchio(self):
-        from gameyamlspiderandgenerator.plugin.itchio import ItchIO
-        obj = ItchIO(link="https://fymm-game.itch.io/ddp")
-        self.assertIsInstance(
-            str(obj.to_yaml()),
-            str,
-        )
+        self.assertGreaterEqual(get_status("https://example.com/"), 200)
+        self.assertIsInstance(get_text("https://example.com/"), str)
 
 
 class HookUnitTest(unittest.TestCase):
