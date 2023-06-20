cdbeeb68c154712ae789f24c4d2c7417bf923a57
kaixinol <keiplyer@gmail.com>
2023-06-20 16:47:14 +0800

    v1.6.5 bugfix

--- gameyamlspiderandgenerator/util/plugin_manager.py
+++ gameyamlspiderandgenerator/util/plugin_manager.py
@@ -37,6 +37,8 @@ class Package:
                 continue
             try:
                 package = f"{base}.{plugin}"
+                if plugin in self["log"]:
+                    continue
                 logger.info(f"Loading {_dir}: {plugin}")
                 temp = importlib.import_module(package)
                 self[_dir][plugin] = [
@@ -44,6 +46,7 @@ class Package:
                     for o in temp.__dict__.values()
                     if isinstance(o, type) and issubclass(o, _type) and o is not _type
                 ][-1]
+                self["log"].append(temp.__name__.split(".")[-1])
             except ImportError as e:
                 logger.trace(e)
                 logger.error(f"Failed to import {_dir}: {plugin}")
@@ -51,14 +54,9 @@ class Package:
                 logger.error(f"Imported {_dir} but no {_type.__name__} found: {plugin}")
 
     def load_plugins(self):
-        print(self.plugin)
-        if not self.plugin:
-            return
         self._load("plugin", BasePlugin)
 
     def load_hooks(self):
-        if not self.hook:
-            return
         if config["hook"] is None:
             if None not in self["log"]:
                 logger.warning(f"All hooks are disabled")
@@ -66,7 +64,10 @@ class Package:
             return
         for plugin in getattr(config, "hook", []):
             try:
+                if plugin in self["log"]:
+                    continue
                 logger.info(f"Loading hook: {plugin}")
+                self["log"].append(plugin)
                 temp = importlib.import_module(f"yamlgenerator_hook_{plugin}")
                 self["hook"][plugin] = [
                     o
