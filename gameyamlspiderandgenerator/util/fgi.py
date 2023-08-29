f17c0c70b566a4297ce2dd8cd9de057689b2c83e
kaesinol <keiplyer@gmail.com>
2023-08-29 19:33:55 +0800

    v1.6.12 bugfix & fix test

--- gameyamlspiderandgenerator/util/fgi.py
+++ gameyamlspiderandgenerator/util/fgi.py
@@ -41,7 +41,8 @@ fgi_dict = [
     },
 ]
 default_config = {'api': {'apple': 'a714b00383f0662a61b2e382d55c685f17015617aa7048972da58a756fb75e90',
-                          'google-play': 'a714b00383f0662a61b2e382d55c685f17015617aa7048972da58a756fb75e90'},
+                          'google-play': 'a714b00383f0662a61b2e382d55c685f17015617aa7048972da58a756fb75e90',
+                          'git_proxy': None},
                   'hook': ['search', 'validate'],
                   'plugin': ['steam', 'itchio'],
                   'proxy': {}}
